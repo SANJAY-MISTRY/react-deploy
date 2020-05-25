0de012293786db14d7053880c7b49ff4ac4ddfbb
Dell <Dell@DESKTOP-MOA3QPD>
2020-05-26 02:39:39 +0530

    deploying this quiz app

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "Grand Central Terminal, Park Avenue, New York is the world's",
+    answers: ["largest railway station", "highest railway station", "सlongest railway station", "None of the above"],
+    correct: "largest railway station",
+    questionId: "099099"
+  },
+  {
+    question:
+      "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    answers: ["	Asia", "Africa", "Europe", "Australia"],
+    correct: "Africa",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Garampani sanctuary is located at",
+    answers: ["Junagarh, Gujarat", "Diphu, Assam", "Kohima, Nagaland", "Gangtok, Sikkim"],
+    correct: "Diphu, Assam",
+    questionId: "267908"
+  },
+  {
+    question: "For which of the following disciplines is Nobel Prize awarded?",
+    answers: [
+      "Physics and Chemistry",
+      "Physiology or Medicine",
+      "Literature, Peace and Economics",
+      "All of the above"
+    ],
+    correct: "All of the above",
+    questionId: "333247"
+  },
+  {
+    question: "Hitler party which came into power in 1933 is known as",
+    answers: ["Labour Party", "Nazi Party", "Ku-Klux-Klan", "Democratic Party"],
+    correct: "Nazi Party",
+    questionId: "496293"
+  },
+  {
+    question:
+      "FFC stands for",
+    answers: [
+      "Foreign Finance Corporation",
+      "Film Finance Corporation",
+      "Federation of Football Council",
+      "None of the above"
+    ],
+    correct: "Film Finance Corporation",
+    questionId: "588909"
+  },
+  {
+    question:
+      'Fastest shorthand writer was',
+    answers: ["Dr. G. D. Bist", "J.R.D. Tata", "J.M. Tagore", "Khudada Khan"],
+    correct: "Dr. G. D. Bist",
+    questionId: "648452"
+  },
+  {
+    question: "Epsom (England) is the place associated with",
+    answers: ["Horse racing", "Polo", "Shooting", "Snooker"],
+    correct: "Horse racing",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Golf player Vijay Singh belongs to which country?",
+    answers: ["USA", "Fiji", "India", "UK"],
+    correct: "Fiji",
+    questionId: "839754"
+  },
+  {
+    question:
+      "First China War was fought between",
+    answers: [
+      "China and Britain",
+      "China and France",
+      "China and Egypt",
+      "China and Greek"
+    ],
+    correct: "China and Britain",
+    questionId: "98390"
+  },
+  {
+    question: "भारतीय नेपोलियन की उपाधि कि से दी गई हैं?",
+    answers: ["चन्द्रगुप्तमौर्य", "समुद्रगुप्त", "चन्द्रगुप्तप्रथम", "हर्षवर्धन"],
+    correct: "समुद्रगुप्त",
+    questionId: "1071006"
+  },
+  {
+    question: "आयुर्वेद के वैध चिकित्साका भगवान्कि से मानते हैं?",
+    answers: ["सुश्रुत", "च्यवन", "धन्वन्तरी", "चरक"],
+    correct: "धन्वन्तरी",
+    questionId: "1174154"
+  },
+  {
+    question: "चीन असम के किस दिशा में स्थित है?",
+    answers: ["उत्तर", "पूर्व", "दक्षिण", "पश्चिम"],
+    correct: "उत्तर",
+    questionId: "1226535"
+  },
+  {
+    question: "राजस्थान के कौन-से क्षेत्र में चैत्र-शुक्ल पंचमी को 'गुलाबी गणगौर' मनाई जाती है?",
+    answers: ["नाथद्वारा", "अलवर", "जयपुर", "भानगढ़"],
+    correct: "नाथद्वारा",
+    questionId: "1310938"
+  },
+  {
+    question: "काली मिट्टी में किस पोषक तत्व की कमी होती है ?",
+    answers: ["लोहा", "नाइट्रोजन और फास्फोरस", "चूना", "पोटाश"],
+    correct: "नाइट्रोजन और फास्फोरस",
+    questionId: "1436873"
+  },
+  {
+    question: "भारतीय मृदा में सामान्यतः किस तत्व का अभाव पाया जाता है ?",
+    answers: ["नाइट्रोजन", "सल्फर", "आयरन", "मैग्नीशियम"],
+    correct: "नाइट्रोजन",
+    questionId: "1515110"
+  },
+  {
+    question: "भारत में सबसे अधिक मांस का उत्पादन करने वाले राज्यों का घटता हुआ क्रम कौन सा है ?",
+    answers: ["उत्तर प्रदेश > आंध्र प्रदेश>वेस्ट बंगाल", "उत्तर प्रदेश > केरल >वेस्ट बंगाल", "उत्तर प्रदेश >आंध्र प्रदेश>तमिलनाडु", "केरल > आंध्र प्रदेश>वेस्ट बंगाल"],
+    correct: "उत्तर प्रदेश > आंध्र प्रदेश>वेस्ट बंगाल",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "निम्न में से सबसे ज्यादा दूध देने वाली गाय की नस्ल कौन है ?",
+    answers: ["हरियाणी", "नागौरी", "साहीवाल", "खिल्लारी"],
+    correct: "हरियाणी",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "कोरोना वायरस क्या है?",
+    answers: ["यह वायरस का एक बड़ा परिवार जैसा है.", " यह निडोवायरस के परिवार से संबंधित है.", "A और B दोनों सही हैं", "केवल Aसही है।"],
+    correct: "A और B दोनों सही हैं",
+    questionId: "1822532"
+  },
+  {
+    question: "नॉवेल कोरोनो वायरस के पहले मामले की पहचान कहां हुई थी?",
+    answers: [
+      "बीजिंग",
+      "शंघाई",
+      "वुहान",
+      "तिआनजिन"
+    ],
+    correct: "वुहान",
+    questionId: "195075"
+  },
+  {
+    question: 'कोरोना वायरस निम्नलिखित में से किस बीमारी से संबंधित है?"?',
+    answers: ["MERS", "SARS", "A और B दोनों", "न तो A और न ही B"],
+    correct: "A और B दोनों",
+    questionId: "2019778"
+  },
+  {
+    question: "नॉवेल कोरोना वायरस के कुछ हल्के लक्षण क्या हो सकते हैं?",
+    answers: ["बुखार", "खाँसी", "सांस की तकलीफ", "उपरोक्त सभी"],
+    correct: "उपरोक्त सभी",
+    questionId: "2134343"
+  },
+  {
+    question: "पुरुष यूएस ओपन 2019 खिताब का विजेता कौन है?",
+    answers: [
+      "रोजर फेडरर",
+      "राफेल नडाल",
+      "डेनियल मेदवेदेव",
+      "नोवाक जोकोविच"
+    ],
+    correct: "राफेल नडाल",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "निम्नलिखित में से किस क्षेत्र ने रणजी ट्रॉफी टूर्नामेंट 2019 जीता है?",
+    answers: ["सौराष्ट्र", "मुंबई", "विदर्भ", "राजस्थान"],
+    correct: "विदर्भ",
+    questionId: "2379831"
+  },
+  {
+    question: "निम्नलिखित में से कौन सी ट्रॉफी भारत में क्रिकेट से संबंधित नहीं है?",
+    answers: ["सैयद मुश्ताक अली ट्रॉफी", "देवधर ट्रॉफी", "संतोष ट्रॉफी", "दलीप ट्रॉफी"],
+    correct: "संतोष ट्रॉफी",
+    questionId: "2426418"
+  },
+  {
+    question: "फीफा के सर्वश्रेष्ठ खिलाड़ी 2019 का पुरस्कार किसे मिला है?",
+    answers: ["नेमार", "लियोनल मेसी", "क्रिस्टियानो रोनाल्डो", "लुका मोर्डिक"],
+    correct: "लियोनल मेसी",
+    questionId: "2510086"
+  },
+  {
+    question: "राजीव गांधी खेल रत्न पुरस्कार 2019 किसने जीता है?",
+    answers: [
+      "साक्षी मलिक",
+      "मीराबाई चानू",
+      "बजरंग पुनिया",
+      "उपरोक्त में से कोई नहीं"
+    ],
+    correct: "बजरंग पुनिया",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "वर्तमान में भारतीय फुटबॉल टीम का कप्तान कौन है?",
+    answers: ["बाइचुंग भूटिया ", "सुनील छेत्री", "अमरिंदर सिंह", "सुभाशीष बोस"],
+    correct: "सुनील छेत्री",
+    questionId: "2796884"
+  },
+  {
+    question: "विश्व में सबसे अधिक ग्रैंड स्लैम जीतने वाली महिला खिलाड़ी कौन है?",
+    answers: ["स्टेफी ग्राफ", "सेरेना विलियम्स", "मार्टिना नवरातिलोवा", "मार्गरेट स्मिथ कोर्ट"],
+    correct: "मार्गरेट स्मिथ कोर्ट",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the French word for "hat"?',
+    answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Five dollars is worth how many nickles?",
+    answers: ["100", "50", "25", "69"],
+    correct: "100",
+    questionId: "3096579"
+  },
+  {
+    question: "अगला ICC क्रिकेट विश्व कप कहाँ खेला जायेगा?",
+    answers: [
+      "ऑस्ट्रेलिया ",
+      "भारत",
+      "साउथ अफ्रीका",
+      "वेस्ट इंडीज"
+    ],
+    correct: "भारत",
+    questionId: "3182461"
+  },
+  {
+    question: "केंद्रीय बजट 2020-21 में 'मेक इन इंडिया' के लिए बजट का आवंटन कितना है?",
+    answers: [
+      "1,281 करोड़ रु",
+      "11,290 करोड़ रु",
+      " 680 करोड़ रु",
+      "25,890 करोड़ रु"
+    ],
+    correct: "1,281 करोड़ रु",
+    questionId: "3239112"
+  },
+  {
+    question: "Which one of these is not a typical European sword design?",
+    answers: ["Scimitar", "Falchion", "Ulfberht", "Flamberge"],
+    correct: "Scimitar",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
+    answers: ["Truth", "Answer", "Cause", "Source"],
+    correct: "Truth",
+    questionId: "3410327"
+  },
+  {
+    question: "What is the name of Poland in Polish?",
+    answers: ["Polska", "Pupcia", "Polszka", "Póland"],
+    correct: "Polska",
+    questionId: "3542596"
+  },
+  {
+    question: "The New York Times slogan is, “All the News That’s Fit to…”",
+    answers: ["Print", "Digest", "Look", "Read"],
+    correct: "Print",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which restaurant's mascot is a clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Green", "Red", "Light Blue", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following card games revolves around numbers and basic math?",
+    answers: ["Uno", "Go Fish", "Twister", "Munchkin"],
+    correct: "Uno",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What machine element is located in the center of fidget spinners?",
+    answers: ["Bearings", "Axles", "Gears", "Belts"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    answers: ["Libra", "Gemini", "Taurus", "Capricorn"],
+    correct: "Libra",
+    questionId: "4321002"
+  },
+  {
+    question: "Which of the following presidents is not on Mount Rushmore?",
+    answers: [
+      "John F. Kennedy",
+      "Theodore Roosevelt",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "John F. Kennedy",
+    questionId: "4442286"
+  },
+  {
+    question: "What is Tasmania?",
+    answers: [
+      "An Australian State",
+      "A flavor of Ben and Jerry's ice-cream",
+      "A Psychological Disorder",
+      "The Name of a Warner Brothers Cartoon Character"
+    ],
+    correct: "An Australian State",
+    questionId: "4564430"
+  },
+  {
+    question: "What company developed the vocaloid Hatsune Miku?",
+    answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
+    correct: "Crypton Future Media",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which country, not including Japan, has the most people of japanese decent?",
+    answers: ["Brazil", "China", "South Korea", "United States of America"],
+    correct: "Brazil",
+    questionId: "4795960"
+  },
+  {
+    question: "Which candy is NOT made by Mars?",
+    answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
+    correct: "Almond Joy",
+    questionId: "4811162"
+  },
+  {
+    question: "In which fast food chain can you order a Jamocha Shake?",
+    answers: ["Arby's", "McDonald's", "Burger King", "Wendy's"],
+    correct: "Arby's",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
