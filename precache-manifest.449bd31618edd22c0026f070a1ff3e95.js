55a262b6681f9a65e137fedcedd74b65187798a9
Dell <Dell@DESKTOP-MOA3QPD>
2020-05-26 02:41:07 +0530

    Updates

---
+++ precache-manifest.449bd31618edd22c0026f070a1ff3e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771a896b24dd12de1c9c6b8ff0a21d8f",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "ac313568364d15914932",
+    "url": "/react-deploy/static/css/main.7fd36747.chunk.css"
+  },
+  {
+    "revision": "33459c622465c06da0ec",
+    "url": "/react-deploy/static/js/2.f6fb56fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.f6fb56fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac313568364d15914932",
+    "url": "/react-deploy/static/js/main.b69d3522.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
